9bce66c63d3cd546097a7b35c177848ca3ba5c5e
Chris West (Faux) <git@goeswhere.com>
2016-03-19 11:43:50 +0000

    find items with :before and :after, and assasinate them via css

--- ext/fixed.js
+++ ext/fixed.js
@@ -1,15 +1,39 @@
+const uniqueClass = 'fix_fixed_c508296f6012bbff2ea27a7e090e457079c72b7974ca85f25cbf9b87e3e6b5cd';
+const styleTagId = 'style_for_' + uniqueClass;
+
 var allNodes = document.getElementsByTagName("*");
 
 var toRemove = [];
 
+const hatred = function (style) {
+    return style.position && 'fixed' == style.position;
+};
+
 for (var i = 0; i < allNodes.length; ++i) {
     var node = allNodes[i];
-    var style = window.getComputedStyle(node);
-    if (style.position && 'fixed' == style.position) {
+    if (hatred(window.getComputedStyle(node))) {
         toRemove.push(node);
     }
+
+    ['::before', '::after'].forEach(function(sub) {
+        if (hatred(window.getComputedStyle(node, sub))) {
+            node.classList.add(uniqueClass);
+        }
+    });
 }
 
 toRemove.forEach(node => {
     node.parentNode.removeChild(node);
 });
+
+if (!document.getElementById(styleTagId)) {
+    const styleTag = document.head.appendChild(document.createElement("style"));
+    styleTag.type = 'text/css';
+    styleTag.id = styleTagId;
+    const css =
+        '.' + uniqueClass + ':before,' +
+        '.' + uniqueClass + ':after { ' +
+        '  position: initial !important' +
+        '}';
+    styleTag.appendChild(document.createTextNode(css));
+}
